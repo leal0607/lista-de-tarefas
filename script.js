c40afeefd43194b7102558c36498af57c7b80b63
VitorLealMarques <vitorleal2040@gmail.com>
2025-08-26 17:12:16 -0300

    Pequenos ajustes no CSS e JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+ // Adicionado comentário para teste de commit
+
 const form = document.getElementById('task-form');
 const input = document.getElementById('task-input');
 const list = document.getElementById('task-list');
